4f830e10544a65576877980a073264cce59ce580
unknown <Saurabh-Web-Dev>
2025-11-20 15:38:55 +0530

    the bootsrap file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000'; // or 127.0.0.1
+axios.defaults.baseURL = 'https://aryanhomoeopharmacy.in'; // or 127.0.0.1
 axios.defaults.withCredentials = true; // VERY IMPORTANT
 
 export default axios;
